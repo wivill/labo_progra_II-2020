10fa62ab0156150f39fce08ad30cc1f36a968be0
Willy <gopwivill@gmail.com>
2020-08-16 16:59:19 -0600

    Basic variable setting.

--- ie0724-lab2/laboratorio2.cpp
+++ ie0724-lab2/laboratorio2.cpp
@@ -1,7 +1,33 @@
+/*
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# IE0724 - Laboratorio 2 - C++: Distancia euclidiana entre puntos.
+# OS: Ubuntu 20.04 Focal Fossa
+# C++: ISO C++17
+#
+# Autor: Willy Villalobos Marrero
+# Correo: willy.villalobos@ucr.ac.cr
+#
+# Descripción: Este programa calcula la distancia euclidiana de una serie 
+# de puntos euclidianos en un espacio de dos dimensiones. La cantida de
+# puntos, así como sus coordenadas, son provistos por el usuario de manera
+# interactiva. El programa devuelve los pares de puntos cuya distancia es
+# mayor y menor, así como los pares de puntos con distancias iguales.
+#
+# Git repo: https://github.com/wivill/labo_progra_II-2020.git
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+*/
+
 #include <iostream>
 using namespace std;
 
+int num_puntos;
+double x1;
+double x2;
+
 int main () {
-    cout << "Hi\n";
+    num_puntos = 2;
+    cout << "Hi " << num_puntos << endl;
     return 0;
 }
