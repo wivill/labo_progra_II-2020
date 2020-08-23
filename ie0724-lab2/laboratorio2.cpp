fbe2d30c9378c6b1e3fd774dd1152c2fe91b15cb
Willy <gopwivill@gmail.com>
2020-08-22 20:20:12 -0600

    Manejo de tipo incorrecto de datos.

--- ie0724-lab2/laboratorio2.cpp
+++ ie0724-lab2/laboratorio2.cpp
@@ -27,11 +27,35 @@ using namespace std;
 // Definición de variables a emplear durante la ejecición del programa.
 int num_puntos;
 float coord_x, coord_y;
+char coma;
 
 int main () {
     cout << "Este es un programa que le ayudará a calcular la distancia máxima y mínima entre cualquier par de puntos, entre una lista de puntos." << endl;
     cout << "¿Cuántos puntos va a introducir?: ";
     cin >> num_puntos;
     cout << num_puntos << endl;
+
+    float puntos[num_puntos][2];
+
+    // Bucle para inicializar los pares oredenados.
+    for (int i = 0; i < num_puntos; i++) {
+        cout << "Digite un punto bidimensional de la forma X.x,Y.y: ";
+        cin >> coord_x;
+        cin >> coma;
+        cin >> coord_y;
+
+        // Revisa si el dato ingresado corresponde al tipo de dato esperado (¿Es un número?).
+        if (!cin) {
+            cerr << "Ha ingresado un valor no permitido." << endl;
+            break; // Valorar cambiar a continue y simplemente dar menos puntos.
+        }
+
+        // Guarda el par ordenado ingresado en la matriz de pares de puntos.
+        puntos[i][0] = coord_x;
+        puntos[i][1] = coord_y;
+
+        cout << "(" << puntos[i][0] << coma << puntos[i][1] <<")" << endl;
+    }
+
     return 0;
 }
